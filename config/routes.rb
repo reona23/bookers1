12b315e9355857e3a1dfef98932da49c044c868a
EC2 Default User <ec2-user@ip-172-31-14-116.ap-northeast-1.compute.internal>
2024-07-11 21:12:15 +0000

    [add] フラッシュメッセージの実装まで

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   #get 'books/edit'
   resources :books
   root 'homes#top'
+
+  get '/books', to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
